2bec756417e83d98e91d2d0fd95266efb6b0ac79
goldenstarsit <goldenstarsit@gmail.com>
2026-09-07 22:22:50 +0500

    perf: cache repository prepared statements

--- src/server/database/repositories/BaseRepository.js
+++ src/server/database/repositories/BaseRepository.js
@@ -1,10 +1,15 @@
 export class BaseRepository {
   constructor(db) {
     this.db = db;
+    this.statementCache = new Map();
   }
 
   prepare(sql) {
-    return this.db.prepare(sql);
+    if (!this.statementCache.has(sql)) {
+      this.statementCache.set(sql, this.db.prepare(sql));
+    }
+
+    return this.statementCache.get(sql);
   }
 
   exec(sql) {
@@ -13,5 +18,9 @@ export class BaseRepository {
 
   transaction(fn) {
     return this.db.transaction(fn);
+  }
+
+  clearStatementCache() {
+    this.statementCache.clear();
   }
 }
